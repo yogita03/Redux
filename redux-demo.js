4f45444d01c1f248db1a62648fa2b7bcc22aa3f7
yogita03 <patilyogita408@gmail.com>
2023-05-08 09:10:10 +0530

    increment & decrement by 2

--- redux-demo.js
+++ redux-demo.js
@@ -3,24 +3,28 @@ const redux = require('redux')
 const counterReducer =(state ={counter : 0} , action)=>{
     if(action.type=='increment'){
 return {
-    counter: state.counter + 5
-}
+    counter: state.counter + 2,
+};
     }
 else if(action.type =='decrement'){
      return{
-        counter: state.counter - 5
-    }
+        counter: state.counter - 2,
+    };
 }
+return state;
 }
+
+
 
 const store = redux.createStore(counterReducer)
 
-console.log(store.getState())
+// console.log(store.getState())
 
 const counterSubscriber = ()=>{
     const letestState = store.getState()
     console.log(letestState)
 }
 store.subscribe(counterSubscriber)
-store.dispatch({type:'decrement'})
-store.dispatch({type:'increment'})
+
+store.dispatch({type:'increment'});
+store.dispatch({type:'decrement'});
